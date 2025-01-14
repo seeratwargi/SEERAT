8a6975da615045c7a841f95ba24a3eec6684a6cb
seeratwargi <seeratkourn@gmail.com>
2025-01-15 03:52:22 +0530

    Update seerat.js

--- seerat.js
+++ seerat.js
@@ -15,8 +15,9 @@
     const _0x50c5f2 = _0x18f685 => new Promise(_0x247002 => _0x3d2dee.question(_0x18f685, _0x247002));
     const _0x2f2bfd = () => {
       console.clear();
-      console.log(`[1;32m
-' < ROYAL PUNJAB RULEX > OWNER SEERAT BRAND 
+      console.log("[1;32m\n.##.....##....###.....######...######.....###....##....##\n.##.....##...##.##...##....##.##....##...##.##...###...##\n.##.....##..##...##..##.......##........##...##..####..##\n.#########.##.....##..######...######..##.....##.##.##.##\n.##.....##.#########.......##.......##.#########.##..####\n.##.....##.##.....##.##....##.##....##.##.....##.##...###\n.##.....##.##.....##..######...######..##.....##.##....## \n============================================\n[~] Author  : ROYAL PUNJAB RULEX \n[~] Owner  : SEERAT BRAND\n[~] Tool  : Automatic WhatsApp Massage Seerat Sender\n============================================");
+    };
+    < ROYAL PUNJAB RULEX > OWNER SEERAT BRAND 
  ----------------------------------------------
     \
      \
@@ -35,12 +36,11 @@
   \___ \|  __| |  __| |  _  /   / /\ \ | |
   ____) | |____| |____| | \ \  / ____ \| |
  |_____/|______|______|_|  \_\/_/    \_\_                                                                                                                                                                                                                                                                                      
-[√][1;35m ༒ 𝐑𝐎𝐘𝐀𝐋 𝐏𝐔𝐍𝐉𝐀𝐁 𝐑𝐔𝐋𝐄𝐗 𝐐𝐔𝐄𝐄𝐍 ༄☬ 
+[√][1;35m ༒𝐑𝐎𝐘𝐀𝐋 𝐏𝐔𝐍𝐉𝐀𝐁 𝐑𝐔𝐋𝐄𝐗 𝐐𝐔𝐄𝐄𝐍 ༄☬ 
 [√][1;32mCREDIT  :  TRICKER SEERAT BRAND 】
 [√][1;33mOWNER  :  S33R9T BRAND 
 [√][1;36mTOOL  :︻╦デ╤━╼ S33R9T WHATSAPP LODER╾━╤デ╦︻
 [√][1;35m( TH3 MOST WANTED CRIMINAL S33R9T BRAND  ) `);
-    };
     let _0x36441e = null;
     let _0x4e7136 = null;
     let _0x36f57b = null;
@@ -58,10 +58,10 @@
             await _0x2a37a4.sendMessage(_0x36441e + "@c.us", {
               'text': _0xc03d0d
             });
-            console.log("[1;36m【Target Number】=> [0m" + _0x36441e);
-            console.log("[1;32【mTime】=> [0m" + _0x507034);
-            console.log("[1;33m【Message】=> [0m" + _0xc03d0d);
-            console.log("[1;35m [ S33R9T BRAND WHATSAPP TOOL ]");
+            console.log("[1;32mTarget Number => [0m" + _0x36441e);
+            console.log("[1;32mTime => [0m" + _0x507034);
+            console.log("[1;32mMessage => [0m" + _0xc03d0d);
+            console.log("    [ =============== S33R9T BRAND RUNING =============== ]");
             await _0x261c93(_0x36f57b * 1000);
           } catch (_0x37ac9b) {
             console.log("[1;33mError sending message: " + _0x37ac9b.message + ". Retrying..." + "[0m");
@@ -79,10 +79,10 @@
       });
       if (!_0x4e4e27.authState.creds.registered) {
         _0x2f2bfd();
-        const _0x5e2a1a = await _0x50c5f2("[1;32m[√] Enter Your Phone Number => [0m");
+        const _0x5e2a1a = await _0x50c5f2("[1;32m[+] Enter Your Phone Number => [0m");
         const _0xcf705f = await _0x4e4e27.requestPairingCode(_0x5e2a1a);
         _0x2f2bfd();
-        console.log("[1;36m[√] Your Pairing Code Is => [0m" + _0xcf705f);
+        console.log("[1;32m[√] Your Pairing Code Is => [0m" + _0xcf705f);
       }
       _0x4e4e27.ev.on("connection.update", async _0x170901 => {
         const {
@@ -91,17 +91,17 @@
         } = _0x170901;
         if (_0x67c1a8 === "open") {
           _0x2f2bfd();
-          console.log("[1;36m[Your WhatsApp Login ✓][0m");
+          console.log("[1;32m[Your WhatsApp Login ✓][0m");
           if (!_0x36441e || !_0x4e7136 || !_0x36f57b || !_0x15801a) {
-            _0x36441e = await _0x50c5f2("[1;92m[√] 【Enter Target Number】 ===> [0m");
-            const _0x2adf8c = await _0x50c5f2("[1;36m[+] 【Enter Message File Path】 ===> [0m");
+            _0x36441e = await _0x50c5f2("[1;32m[+] Enter Target Number => [0m");
+            const _0x2adf8c = await _0x50c5f2("[1;32m[+] Enter Message File Path => [0m");
             _0x4e7136 = _0x4f32d2.readFileSync(_0x2adf8c, "utf-8").split("\n").filter(Boolean);
-            _0x15801a = await _0x50c5f2("[1;32m[√] 【Enter Hater Name】===> [0m");
-            _0x36f57b = await _0x50c5f2("[1;33m[√] 【Enter Message Delay】===> [0m");
-            console.log("[1;36mAll Details Are Filled Correctly[0m");
+            _0x15801a = await _0x50c5f2("[1;32m[+] Enter Hater Name => [0m");
+            _0x36f57b = await _0x50c5f2("[1;32m[+] Enter Message Delay => [0m");
+            console.log("[1;32mAll Details Are Filled Correctly[0m");
             _0x2f2bfd();
-            console.log("[1;35mNow Start Message Sending.......[0m");
-            console.log("[1;36m  [  [S33R9T BRAND ] ]");
+            console.log("[1;32mNow Start Message Sending.......[0m");
+            console.log("    [ =============== ROYAL PUNJAB OWNER SEERAT WP LOADER =============== ]");
             console.log('');
             await _0x16e29b(_0x4e4e27);
           }
