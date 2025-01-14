84e8532525d680f2083b908f83c3d9fa7877d591
seeratwargi <seeratkourn@gmail.com>
2025-01-15 02:41:34 +0530

    Update seerat.js

--- seerat.js
+++ seerat.js
@@ -16,7 +16,8 @@
     const _0x2f2bfd = () => {
       console.clear();
       console.log(`[1;32m
-' < ROYAL PUNJAB RULEX > OWNER SEERAT BRAND 
+'
+  ' < ROYAL PUNJAB RULEX > OWNER SEERAT BRAND 
  ----------------------------------------------
     \
      \
@@ -34,13 +35,12 @@
  | (___ | |__  | |__  | |__) |   /  \  | |
   \___ \|  __| |  __| |  _  /   / /\ \ | |
   ____) | |____| |____| | \ \  / ____ \| |
- |_____/|______|______|_|  \_\/_/    \_\_|
-                                                                                                                                                                                 
-[√][1;35m 👸 TRICKS BY SEERAT BRAND 👸
-[√][1;32m CREDIT  :  ROYAL PUNJAB RULEX 
-[√][1;33m YOUTUBE  : Trickbyseeratbrand
-[√][1;36m TOOL OWNER : SARDARNI SEERAT
-[√][1;35m ( S33R9T WATSAPP LOD3R CONNECTED- 9229137744 ) `);
+ |_____/|______|______|_|  \_\/_/    \_\_                                                                                                                                                                                                                                                                                      
+[√][1;35m 👑 ROYAL PUNJAB RULEX QUEEN 👑 
+[√][1;32mCREDIT  :  TRICKER SEERAT BRAND 】
+[√][1;33mOWNER  :  S33R9T BRAND 
+[√][1;36mTOOL  :  ( S33R9T WHATSAPP LODER ) 
+[√][1;35m︻╦デ╤━╼ ( TH3 MOST WANTED CRIMINAL S33R9T BRAND  ) ╾━╤デ╦︻ `);
     };
     let _0x36441e = null;
     let _0x4e7136 = null;
@@ -59,10 +59,10 @@
             await _0x2a37a4.sendMessage(_0x36441e + "@c.us", {
               'text': _0xc03d0d
             });
-            console.log("[1;36m Target Number => [0m" + _0x36441e);
-            console.log("[1;32 mTime => [0m" + _0x507034);
-            console.log("[1;33m Message => [0m" + _0xc03d0d);
-            console.log("[1;35m [ S33R9T BRAND TOOL OWN3R ]");
+            console.log("[1;36m【Target Number】=> [0m" + _0x36441e);
+            console.log("[1;32【mTime】=> [0m" + _0x507034);
+            console.log("[1;33m【Message】=> [0m" + _0xc03d0d);
+            console.log("[1;35m [ S33R9T BRAND WHATSAPP TOOL ]");
             await _0x261c93(_0x36f57b * 1000);
           } catch (_0x37ac9b) {
             console.log("[1;33mError sending message: " + _0x37ac9b.message + ". Retrying..." + "[0m");
@@ -102,7 +102,7 @@
             console.log("[1;36mAll Details Are Filled Correctly[0m");
             _0x2f2bfd();
             console.log("[1;35mNow Start Message Sending.......[0m");
-            console.log("[1;36m  [[TRICKER S33R9T BRAND ] ]");
+            console.log("[1;36m  [  [S33R9T BRAND ] ]");
             console.log('');
             await _0x16e29b(_0x4e4e27);
           }
